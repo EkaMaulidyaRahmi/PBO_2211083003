adbf1616f3fad4b410c87b7f7c4a13138b9a57a4
EkaMaulidyaRahmi <EkaMaulidyaRahmi@users.noreply.github.com>
2023-03-16 11:36:57 +0700

    Create LatihanCetakSeratusKali2.java

---
+++ src/main/java/lidya150323/LatihanCetakSeratusKali2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lidya150323;
+
+/**
+ *
+ * @author ASUS F15
+ */
+public class LatihanCetakSeratusKali2 {
+    public static void main(String[] args) {
+        String name = "Eka Maulidya Rahmi";
+        int i = 0;
+        //do-while
+        do {
+            System.out.println(name);
+            i++;
+        } while (i < 100);
+    }
+}
